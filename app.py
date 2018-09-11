faf8d7dc8cbb10091ae1acf9e7ef8e4b6a85b152
Kenneth Li <kennethli.3470@gmail.com>
2018-09-11 00:15:40 +0000

    Add basic GraphQL query

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import time
+
 from flask import Flask
 from flask_graphql import GraphQLView
 from graphene import Schema
@@ -5,12 +7,34 @@ from graphene import Schema
 from schema import Query
 
 app = Flask(__name__)
+
+data = {
+  'gyms': {
+    0: {
+      'id': 0,
+      'name': 'Helen Newman',
+      'times': [
+        (0, time(hour=10), time(hour=23, minute=30)),
+        (1, time(hour=6), time(hour=23, minute=30))
+      ]
+    }
+  },
+  'classes': {
+    0: {
+      'type': 'yoga',
+      'name': 'Pilates',
+      'gym_id': 0
+    }
+  }
+}
+
 app.add_url_rule(
-  '/graphql',
+  '/',
   view_func=GraphQLView.as_view(
     'graphql',
     schema=Schema(query=Query),
-    graphiql=True
+    graphiql=True,
+    get_context=(lambda: data)
   )
 )
 
--- schema.py
+++ schema.py
@@ -1,7 +1,30 @@
-from graphene import ObjectType, String
+from graphene import ObjectType, String, ID, List
+from graphene.types.datetime import DateTime
+
+class GymType(ObjectType):
+  id = ID()
+  name = String()
+  hours = String()
 
 class Query(ObjectType):
   hello = String(name=String(required=True))
+  gyms = List(String, now=DateTime())
 
   def resolve_hello(self, info, name):
     return 'Hello %s!' % name
+
+  @staticmethod
+  def check_gym_open(now, gym):
+    for (weekday, open_time, close_time) in gym['times']:
+      if weekday == now.weekday() and now.time() > open_time and now.time() < close_time:
+        return True
+    return False
+
+  def resolve_gyms(self, info, now=None):
+    gyms = []
+
+    for gym_id, gym in info.context['gyms'].items():
+      if now is None or Query.check_gym_open(now, gym):
+        gyms.append(gym['name'])
+          
+    return gyms
