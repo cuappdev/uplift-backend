bc3919b96d43b514da09e9d0299a8ef669752f86
Kenneth Li <kennethli.3470@gmail.com>
2018-09-15 19:28:54 +0000

    Flesh out GraphQL queries for classes and gyms with dummy data

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from datetime import time
-
+from datetime import time, datetime
 from flask import Flask
 from flask_graphql import GraphQLView
 from graphene import Schema
-
 from schema import Query
 
 app = Flask(__name__)
@@ -13,17 +11,31 @@ data = {
     0: {
       'id': 0,
       'name': 'Helen Newman',
+      'description': 'helen newman description',
       'times': [
         (0, time(hour=10), time(hour=23, minute=30)),
         (1, time(hour=6), time(hour=23, minute=30))
+      ],
+      'popular': [
+        [0,0,0,0,0,0,0,0,0,0,19,31,32,23,26,43,59,57,51,51,47,34,17,3],
+        [0,0,0,0,0,0,15,25,27,22,21,31,47,53,45,34,36,52,70,75,60,35,14,0]
       ]
+    }
+  },
+  'class_details': {
+    0: {
+      'tags': ['yoga'],
+      'name': 'Pilates',
+      'description': 'pilates description'
     }
   },
   'classes': {
     0: {
-      'type': 'yoga',
-      'name': 'Pilates',
-      'gym_id': 0
+      'instructor': 'person A',
+      'gym_id': 0,
+      'class_description_id': 0,
+      'start_time': datetime(year=2018, month=9, day=10, hour=23, minute=30),
+      'end_time': datetime(year=2018, month=9, day=11)
     }
   }
 }
--- schema.py
+++ schema.py
@@ -1,30 +1,78 @@
-from graphene import ObjectType, String, ID, List
+from graphene import InputObjectType, ObjectType, String, List, Int, InputField
 from graphene.types.datetime import DateTime
+import utils
 
 class GymType(ObjectType):
-  id = ID()
+  id = Int()
   name = String()
-  hours = String()
+  description = String()
+  popular = List(List(Int))
+
+  def __init__(self, gym):
+    self.id = gym['id']
+    self.name = gym['name']
+    self.description = gym['description']
+    self.popular = gym['popular']
+
+class ClassType(ObjectType):
+  name = String()
+  gym_id = Int()
+  description = String()
+  start_time = DateTime()
+  end_time = DateTime()
+  instructor = String()
+  tags = List(String)
+
+  def __init__(self, class_data, class_details_data):
+    self.instructor = class_data['instructor']
+    self.gym_id = class_data['gym_id']
+    self.description = class_details_data['description']
+    self.name = class_details_data['name']
+    self.tags = class_details_data['tags']
+    self.start_time = class_data['start_time']
+    self.end_time = class_data['end_time']
 
 class Query(ObjectType):
-  hello = String(name=String(required=True))
-  gyms = List(String, now=DateTime())
+  gyms = List(GymType, now=DateTime(), gym_id=Int(name='id'))
+  classes = List(
+    ClassType,
+    now=DateTime(),
+    tags=List(String),
+    gym_id=Int(),
+    instructor=String()
+  )
 
-  def resolve_hello(self, info, name):
-    return 'Hello %s!' % name
+  def resolve_gyms(self, info, now=None, gym_id=None):
+    gyms_data = info.context['gyms']
 
-  @staticmethod
-  def check_gym_open(now, gym):
-    for (weekday, open_time, close_time) in gym['times']:
-      if weekday == now.weekday() and now.time() > open_time and now.time() < close_time:
-        return True
-    return False
+    if gym_id is not None:
+      if gym_id in gyms_data:
+        return [GymType(gyms_data[gym_id])]
+      return None
 
-  def resolve_gyms(self, info, now=None):
     gyms = []
 
-    for gym_id, gym in info.context['gyms'].items():
-      if now is None or Query.check_gym_open(now, gym):
-        gyms.append(gym['name'])
-          
+    for gym_id, gym_data in gyms_data.items():
+      if now is None or utils.is_gym_open(now, gym_data):
+        gyms.append(GymType(gym_data))
+
     return gyms
+
+  def resolve_classes(self, info,
+                      now=None, tags=None,
+                      gym_id=None, instructor=None):
+    classes_data = info.context['classes']
+    class_details_data = info.context['class_details']
+    classes = []
+
+    for class_id, class_data in classes_data.items():
+      class_detail_data = class_details_data[class_data['class_description_id']]
+
+      if ((now is None or now.date() == class_data['start_time'].date()) \
+          and (tags is None \
+              or any([tag in class_detail_data['tags'] for tag in tags])) \
+          and (gym_id is None or gym_id == class_data['gym_id'])
+          and (instructor is None or instructor == class_data['instructor'])):
+        classes.append(ClassType(class_data, class_detail_data))
+
+    return classes
