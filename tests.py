39ad5501aa443ba424d0e5de5a479dc2e52cdc90
johnathananderson <andersonjohnathan@yahoo.com>
2019-11-07 22:33:25 -0500

    Remove restrictions from snapshot tests

--- tests.py
+++ tests.py
@@ -35,7 +35,6 @@ class TestQuery(TestCase):
                             timeRanges {
                                 startTime
                                 endTime
-                                restrictions
                             }
                         }
                         items
