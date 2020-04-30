c00e388e11bbc2bfc93af1e0a593aa26eb2c2011
williambaibai <williambaibai@gmail.com>
2020-04-30 11:46:16 -0400

    Profile endpoint

--- src/app/controllers/get_games_by_user.py
+++ src/app/controllers/get_games_by_user.py
@@ -1,4 +1,5 @@
 from . import *
+from datetime import datetime as dt
 
 
 class GetGamesByUserController(AppDevController):
@@ -13,4 +14,17 @@ class GetGamesByUserController(AppDevController):
         user = users_dao.get_user_by_id(user_id)
 
         serialized_games = [game_dao.serialize_game(game) for game in user.games]
-        return utils.success_response(serialized_games)
+        result = {"my_games": [], "joined_games": [], "past_games": []}
+        for game in serialized_games:
+            game_time = utils.parse_datetime(game["time"])
+            tz_info = game_time.tzinfo
+            if game_time < dt.now(tz=tz_info):
+                result["past_games"].append(game)
+            else:
+                creator_id = game["players"][0]
+                if int(creator_id) == int(user_id):
+                    result["my_games"].append(game)
+                else:
+                    result["joined_games"].append(game)
+
+        return utils.success_response(result)
