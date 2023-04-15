d8dd16db45be7751ee1a9146ef674b25cbcc8d63
Marya Kim <mnk45@cornell.edu>
2023-04-15 16:32:16 -0400

    Merge bella's gym model with marya's capacity model and scrapers. Also, add location info to gym model.

--- src/app2/app.py
+++ src/app2/app.py
@@ -1,11 +1,14 @@
 from database import db_session, init_db
 from flask import Flask
-from schema import schema
-
+from flask_graphql import GraphQLView
+from graphene import Schema
+from schema import schema, Query
 from flask_graphql import GraphQLView
 
 app = Flask(__name__)
 app.debug = True
+
+schema = Schema(query=Query)
 
 app.add_url_rule("/graphql", view_func=GraphQLView.as_view("graphql", schema=schema, graphiql=True))
 
@@ -13,6 +16,14 @@ app.add_url_rule("/graphql", view_func=GraphQLView.as_view("graphql", schema=sch
 def shutdown_session(exception=None):
     db_session.remove()
 
+# Init gym tables and populate them with basic gym information
+init_db()
+db_session.commit()
+
+# scrape C2C website for latest capacity
+from c2c_scraper import scrape_capacity
+scrape_capacity()
+db_session.commit()
+
 if __name__ == "__main__":
-    init_db()
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
