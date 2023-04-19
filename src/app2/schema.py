90a8334465ebe628fabd29088301aa6a260f9457
Isabella Hoie <iah27@cornell.edu>
2023-04-19 19:27:10 -0400

    Implemented PR changes in schema.py and added Price and Amenity class and price resolver in Activity class

--- src/app2/schema.py
+++ src/app2/schema.py
@@ -1,6 +1,7 @@
 from models.gym import Gym as GymModel, GymTime as GymTimeModel
 from models.daytime import DayTime as DayTimeModel
 from models.activity import Activity as ActivityModel, Price as PriceModel
+from models.activity import Amenity as AmenityModel
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType
@@ -8,11 +9,9 @@ from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType
 class Gym(SQLAlchemyObjectType):
     class Meta:
         model = GymModel
-        interfaces = (relay.Node,)
 
     times = graphene.List(lambda: DayTime, day=graphene.Int(), start_time=graphene.DateTime(), end_time=graphene.DateTime(), restrictions=graphene.String(), special_hours=graphene.Boolean())
     activities = graphene.List(lambda: Activity, name=graphene.String())
-    #capacity = graphene.List(lambda: Capacity)
 
     def resolve_times(self, info, day=None, start_time=None, end_time=None):
         query = GymTime.get_query(info=info)
@@ -38,10 +37,7 @@ class Gym(SQLAlchemyObjectType):
         activity_queries = []
         for act in self.activities:
             activity = query.filter(ActivityModel.id == act.id)
-            if activity.first():
-                if name and act.name == name:
-                    activity_queries.append(activity[0])
-                elif not name:
+            if activity.first() and (name == act.name or name == None):
                     activity_queries.append(activity[0])
         return activity_queries
         
@@ -58,23 +54,37 @@ class Activity(SQLAlchemyObjectType):
         model = ActivityModel
 
     gyms = graphene.List(lambda: Gym, name=graphene.String())
+    prices = graphene.List(lambda: Price, cost=graphene.Int(), one_time=graphene.Boolean())
 
     def resolve_gyms(self, info, name=None):
         query = Gym.get_query(info=info)
         gym_queries = []
         for g in self.gyms:
             gym = query.filter(GymModel.id == g.id)
-            if gym.first():
-                if name and g.name == name:
-                    gym_queries.append(gym[0])
-                elif not name:
-                    gym_queries.append(gym[0])
+            if gym.first() and (name == g.name or name == None):
+                gym_queries.append(gym[0])
+              
         return gym_queries
+
+    def resolve_prices(self, info, cost=None, one_time=None):
+      query = Price.get_query(info=info)
+      query = query.filter(PriceModel.activity_id == self.id)
+      if cost:
+        query = query.filter(PriceModel.cost == cost)
+      if one_time != None:
+        query = query.filter(PriceModel.one_time == one_time)
+      return query
+
+class Price(SQLAlchemyObjectType):
+  class Meta:
+    model = PriceModel
+
+class Amenity(SQLAlchemyObjectType):
+  class Meta:
+    model = AmenityModel
 
 
 class Query(graphene.ObjectType):
-    # node = relay.Node.Field()
-    # allowing single column sorting
 
     gyms = graphene.List(lambda: Gym, name=graphene.String(), description=graphene.String(), location=graphene.String(),image_url=graphene.String())
 
@@ -84,8 +94,8 @@ class Query(graphene.ObjectType):
         query=query.filter(GymModel.name == name)
       return query.all()
 
-    activities = graphene.List(lambda: Activity, name=graphene.String(
-    ), details=graphene.String(), image_url=graphene.String())
+    activities = graphene.List(lambda: Activity, name=graphene.String( \
+      ), details=graphene.String(), image_url=graphene.String())
 
     def resolve_activities(self, info, name=None):
         query = Activity.get_query(info)
