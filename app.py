cfb44d5aa0ee7d703300811fae43abf79d347c24
aldenlamp <aldenblamp@gmail.com>
2023-09-11 18:49:18 -0400

    Address comments

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_graphql import GraphQLView
 from graphene import Schema
+from graphql.utils import schema_printer
 from src.database import db_session, init_db
 from src.schema import Query
 from src.constants import create_gym_table
+
 
 app = Flask(__name__)
 app.debug = True
@@ -20,7 +22,12 @@ def shutdown_session(exception=None):
 init_db()
 create_gym_table()
 
+# Create schema.graphql
+with open("schema.graphql", "w+") as schema_file:
+    schema_file.write(schema_printer.print_schema(schema))
+    schema_file.close()
 
 # Should only be used for dev
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000) # For Dev Purposes only (use start_server.sh for release)
+
