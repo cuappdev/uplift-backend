50af6a6e3ea02b06cfb146473c3f2522894a2c93
Joyce Wu <jyw55@cornell.edu>
2023-09-16 18:53:16 -0400

    Postgres changes

--- src/database.py
+++ src/database.py
@@ -1,10 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 # engine = create_engine("sqlite:///database.sqlite3")
-engine = create_engine("sqlite:///test.sqlite3") # Soley for temp dev testing
+db_user = os.environ.get("DB_USERNAME")
+db_password = os.environ.get("DB_PASSWORD")
+db_name = os.environ.get("DB_NAME")
+
+db_url = f"postgresql://{db_user}:{db_password}@localhost:5432/{db_name}"
+engine = create_engine(db_url) # Soley for temp dev testing
 db_session = scoped_session(
   sessionmaker(autocommit=False, autoflush=False, bind=engine)
 )
