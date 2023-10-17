6319953d8e803da7a3eedf648ccf3149fe69c3f6
Isabella Hoie <iah27@cornell.edu>
2023-10-17 00:10:22 -0400

    fixed facility.py

--- src/models/facility.py
+++ src/models/facility.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD:src/models/facility.py
-<<<<<<<< HEAD:src/models/facility.py
+
 import enum
 from sqlalchemy import Column, ForeignKey, String, Enum, Integer
-========
-import datetime
-from importlib.machinery import FrozenImporter
-from typing import Counter
-from database import Base
-from sqlalchemy import Table, Column, Integer, DateTime, ForeignKey, String, Boolean, null
->>>>>>>> 2eebca9 (almost implementing Facilities):src/uplift/models/activity.py
 from sqlalchemy.orm import relationship
 from src.database import Base
 from src.models.openhours import OpenHours
@@ -25,7 +17,7 @@ class Facility(Base):
     id = Column(Integer, primary_key=True)
     gym_id = Column(Integer, ForeignKey("gym.id"), nullable=False)
     name = Column(String(), nullable=False)
-<<<<<<<< HEAD:src/models/facility.py
+
     facility_type = Column(Enum(FacilityType), nullable=False)
     open_hours = relationship("OpenHours")
     capacities = relationship("Capacity")
@@ -34,14 +26,9 @@ class Facility(Base):
     # prices = relationship('Price', cascade='delete, all')
     # amenities = relationship('Amenity', cascade='delete, all')
     # image_url = Column(String(1000), nullable=True)
-========
-    details = Column(String(), nullable=False)
-    gyms = relationship('Gym', secondary=activities_to_gyms,
-                        back_populates="activities")
-    prices = relationship('ActivityPrice', cascade='delete, all')
-    amenities = relationship('Amenity', cascade='delete, all')
-    image_url = Column(String(), nullable=True)
->>>>>>>> 2eebca9 (almost implementing Facilities):src/uplift/models/activity.py
+
+    
+
 
     def __init__(self, **kwargs):
         self.id = kwargs.get("id")
@@ -61,7 +48,7 @@ class Facility(Base):
             "capacities": self.capacities,
         }
 
-<<<<<<<< HEAD:src/models/facility.py
+
 
 # Left here as a reference for the above TODOs
 
@@ -94,71 +81,4 @@ class Amenity(Base):
         self.image_url = kwargs.get("image_url")
         self.activity_id = kwargs.get("activity_id")
 """
-=======
-from ast import For
-from database import Base
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, Float, String, func
-from sqlalchemy.orm import backref, relationship
-from models.price import Price
 
-
-class Facility(Base):
-  __tablename__ = "facility"
-
-  id = Column(Integer, primary_key=True)
-  name = Column(String(), nullable=False)
-  times = relationship('FacilityTime', cascade='delete, all')
-  price = relationship('Price', cascade='delete, all')
-  equipment = relationship('Equipment', cascade='delete, all') 
-  image_url = Column(String(), nullable=True)
-
-  def __init__(self, **kwargs):
-    self.name = kwargs.get("name")
-    self.image_url = kwargs.get("image")
-
-
-class FacilityTime(Base):
-  __tablename__ = "facilitytime"
-
-  id = Column(Integer, primary_key=True)
-  facilty_id = Column(Integer, ForeignKey('facility.id'), nullable=False)
-  daytime_id = Column(Integer, ForeignKey('daytime.id'), nullable=False)
-
-  def __init__(self, **kwargs):
-    self.facility_id = kwargs.get("facility_id")
-    self.daytime_id = kwargs.get("daytime_id")
-
-class FacilityPrice(Base):
-  __tablename__ = "facilityprice"
-
-  id = Column(Integer, primary_key=True)
-  facility_id = Column(Integer, ForeignKey('facility.id'), nullable=False)
-  price_id = Column(Integer, ForeignKey('price.id'), nullable=False)
-
-  def __init__(self, **kwargs):
-    self.facility_id=kwargs.get("facility_id")
-    self.price_id=kwargs.get("price_id")
-
-
-class Equipment(Base):
-  __tablename__ = "equipment"
-
-  id = Column(Integer, primary_key=True)
-  equipment_type = Column(String(), nullable=False)
-  name = Column(String(), nullable=False)
-  quantity = Column(Integer, nullable=False)
-  facility_id = Column(Integer, ForeignKey('facility.id'), nullable=False)
-
-  def __init__(self, **kwargs):
-    self.equipment_type = kwargs.get("equipment_type")
-    self.name = kwargs.get("name")
-    self.quantity = kwargs.get("quantity")
-    self.facility_id=kwargs.get("facility_id")
-
-
-
-
-
-
-
->>>>>>> 2eebca9 (almost implementing Facilities):src/uplift/models/facility.py
