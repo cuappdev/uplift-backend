3b56d88974df79e616d19bec85d5f6d184f1295a
Kidus Zegeye <51487468+kidzegeye@users.noreply.github.com>
2023-10-17 20:00:53 -0400

    Implement Group Class Scraper (#110)
    * Created activity model and many to many relationship with gym model; added activity and gym rresolvers to the schema
    
    * added activity model to import statements on init_db()
    
    * deleted testing objects in init_db()
    
    * deleted duplicate code
    
    * Implemented PR edits in activity.py
    
    * Implemented PR edits in gym.py
    
    * Implemented PR changes in schema.py and added Price and Amenity class and price resolver in Activity class
    
    * Populated models to test
    
    * Cleaning up some merge conflicts
    
    * almost implementing Facilities
    
    * Kidus initial commit
    
    * Fix schema
    
    * Update scraper with new page formatting
    
    * Disambiguify var names
    
    * Implement class model
    
    * Remove old folders
    
    * Move class fields to assoc table
    
    * Remove price class
    
    * Modify Class-Gym relation, add index.html
    
    * Implement group class scraping
    
    ---------
    
    Co-authored-by: Isabella Hoie <iah27@cornell.edu>
    Co-authored-by: isabellahoie <68919125+isabellahoie@users.noreply.github.com>

--- src/models/classes.py
+++ src/models/classes.py
@@ -1,5 +1,6 @@
 import datetime
-from database import Base
+
+from ..database import Base
 from sqlalchemy import (
     Table,
     Column,
@@ -13,31 +14,13 @@ from sqlalchemy import (
 )
 from sqlalchemy.orm import backref, relationship
 
-classes_to_gyms = Table(
-    "classes_to_gyms",
-    Base.metadata,
-    Column("id", primary_key=True),
-    Column("gym_id", ForeignKey("gym.id")),
-    Column("class_id", ForeignKey("class.id")),
-    Column("location", String()),
-    Column("instructor", String()),
-    Column("isCancelled", Boolean()),
-    Column("start_time", DateTime()),
-    Column("end_time", DateTime()),
-)
-
-
 class Class(Base):
     __tablename__ = "class"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(), nullable=False)
     description = Column(String(), nullable=False)
-    gyms = relationship(
-        "Gym",
-        secondary=classes_to_gyms,
-        back_populates="classes",  ##Change gym to rec_center
-    )
+    gyms = relationship("ClassInstance", back_populates="class_")
 
     def __init__(self, **kwargs):
         self.id = kwargs.get("id")
@@ -49,4 +32,43 @@ class Class(Base):
             "id": self.id,
             "name": self.name,
             "description": self.description,
+        }
+
+class ClassInstance(Base):
+    __tablename__ = "class_instance"
+
+    id = Column(Integer, primary_key=True)
+    gym_id = Column(Integer, ForeignKey("gym.id"), nullable=True)
+    class_id = Column(Integer, ForeignKey("class.id"), nullable=False)
+    location = Column(String(), nullable=False)
+    instructor = Column(String(), nullable=False)
+    isCanceled = Column(Boolean(), nullable=False, default=False)
+    isVirtual = Column(Boolean(), nullable=False, default=False)
+    start_time = Column(DateTime(), nullable=False)
+    end_time = Column(DateTime(), nullable=False)
+    class_ = relationship("Class", back_populates="gyms")
+    gym = relationship("Gym", back_populates="classes")
+
+    def __init__(self, **kwargs):
+        self.id = kwargs.get("id")
+        self.gym_id = kwargs.get("gym_id")
+        self.class_id = kwargs.get("class_id")
+        self.location = kwargs.get("location")
+        self.instructor = kwargs.get("instructor")
+        self.isCanceled = kwargs.get("isCanceled")
+        self.isVirtual = kwargs.get("isVirtual")
+        self.start_time = kwargs.get("start_time")
+        self.end_time = kwargs.get("end_time")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "gym_id": self.gym_id,
+            "class_id": self.class_id,
+            "location": self.location,
+            "instructor": self.instructor,
+            "isCanceled": self.isCanceled,
+            "isVirtual": self.isVirtual,
+            "start_time": self.start_time,
+            "end_time": self.end_time,
         }
