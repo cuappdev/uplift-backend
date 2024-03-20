08bc9e91ba1f3fae44bba06158b81ab483a8f3cc
Daisy Chang <daisychang.m@gmail.com>
2024-03-20 17:28:33 -0400

    Created Activities model

--- src/models/gym.py
+++ src/models/gym.py
@@ -15,6 +15,7 @@ class Gym(Base):
         - `amenities`       (nullable) This gym's amenities.
         - `facilities`      (nullable) This gym's facilities.
         - `hours`           (nullable) The building hours.
+        - `activities`      (nullable) The gym's activities.
         - `image_url`       The URL of this gym's image.
         - `latitude`        The latitude coordinate of this gym.
         - `longitude`       The longitude coordinate of this gym.
@@ -29,6 +30,7 @@ class Gym(Base):
     amenities = relationship("Amenity")
     facilities = relationship("Facility")
     hours = relationship("OpenHours")
+    activities = relationship("Activity")
     image_url = Column(String, nullable=True)
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
