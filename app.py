6c38e348df138d1bf9e0a8f89481d1df009a06d5
Joshua Dirga <joshdirga@gmail.com>
2024-11-26 14:15:25 -0500

    Readded scraping comments

--- app.py
+++ app.py
@@ -84,6 +84,7 @@ def shutdown_session(exception=None):
 
 # Only define scheduler tasks if not in migration mode
 if not FLASK_MIGRATE:
+    # Scrape capacities every 15 minutes
     @scheduler.task("interval", id="scrape_hours", seconds=900)
     def scrape_hours():
         try:
@@ -97,6 +98,7 @@ if not FLASK_MIGRATE:
         except Exception as e:
             logging.error(f"Error in scrape_hours: {e}")
 
+    # Scrape capacities every 10 minutes
     @scheduler.task("interval", id="scrape_capacities", seconds=600)
     def scrape_capacities():
         try:
@@ -105,6 +107,7 @@ if not FLASK_MIGRATE:
         except Exception as e:
             logging.error(f"Error in scrape_capacities: {e}")
 
+    # Scrape capacities every hour
     @scheduler.task("interval", id="scrape_classes", seconds=3600)
     def scrape_classes():
         try:
