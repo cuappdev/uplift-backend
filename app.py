3d9ed74276e220b37774e03849d7adbb37b15024
Joshua Dirga <joshdirga@gmail.com>
2025-02-26 17:58:22 -0500

    Removed gunicorn and only run sentry on dev/prod

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from app_factory import create_app
 import sentry_sdk
+import os
 
-
-# Initialize Sentry
-sentry_sdk.init(
-    dsn="https://2a96f65cca45d8a7c3ffc3b878d4346b@o4507365244010496.ingest.us.sentry.io/4507850536386560",
-    traces_sample_rate=1.0,
-    profiles_sample_rate=1.0,
-)
+# Initialize Sentry only if not in local
+if os.environ.get('FLASK_ENV') in ["development", "production"]:
+    sentry_sdk.init(
+        dsn="https://2a96f65cca45d8a7c3ffc3b878d4346b@o4507365244010496.ingest.us.sentry.io/4507850536386560",
+        traces_sample_rate=1.0,
+        profiles_sample_rate=1.0,
+    )
 
 # Create Flask app with scrapers enabled
 app = create_app(run_migrations=False)
